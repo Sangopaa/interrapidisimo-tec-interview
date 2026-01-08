bff91c43c570920d069fd3b3c37ace5b5cdb4b35
Sangopaa <Sangopaa@gmail.com>
2026-01-08 02:42:30 -0500

    implementing lazy loading and reloading modules

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { baseUrlInterceptor } from './core/interceptors/base-url.interceptor';
 
@@ -8,7 +8,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
     provideHttpClient(withInterceptors([baseUrlInterceptor])),
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes } from '@angular/router';
-import { Posts } from './features/posts/posts';
 
 export const routes: Routes = [
-  { path: 'posts', component: Posts },
+  { path: 'posts', loadComponent: () => import('./features/posts/posts').then((m) => m.Posts) },
   { path: '', redirectTo: 'posts', pathMatch: 'full' },
   { path: '**', redirectTo: 'posts' },
 ];
